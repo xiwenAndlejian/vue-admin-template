3deab7db1c2d4dbcf8e4c3230509d579bc6efcb0
xiwenAndlejian <94333236@qq.com>
2018-09-16 19:20:03 +0800

    图片上传

---
+++ src/utils/imageUpload.js
@@ -0,0 +1,28 @@
+
+const image = ['image/jpeg', 'image/png']
+export function beforeAvatarUpload(file) {
+  console.log('before')
+  const isJPG = image.find(value => {
+    return value === file.type
+  }) !== undefined
+  const isLt2M = file.size / 1024 / 1024 < 2
+  console.log(file)
+  if (!isJPG) {
+    this.$message.error('上传头像图片只能是 JPG/PNG 格式!')
+  }
+  if (!isLt2M) {
+    this.$message.error('上传头像图片大小不能超过 2MB!')
+  }
+  return isJPG && isLt2M
+}
+
+export function avatarUploadSuccess(response, file, fileList) {
+  console.log(response)
+  console.log(file)
+  debugger
+  this.userInfo.avatar = URL.createObjectURL(file.raw)
+}
+
+export function avatarUpload() {
+  console.log(1)
+}
