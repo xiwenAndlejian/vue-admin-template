625af230703e8203e3a4f41767379354ae2283c7
xiwenAndlejian <94333236@qq.com>
2018-09-19 22:00:31 +0800

    用户详情修改api

--- src/api/user.js
+++ src/api/user.js
@@ -6,3 +6,11 @@ export function getUserDetail(userId) {
     method: 'get'
   })
 }
+
+export function saveUserDetail(userId, data) {
+  return request({
+    url: `/user/${userId}`,
+    method: 'put',
+    data: data
+  })
+}
