984048614359de134cd86df4f0e40d1b58c8de8c
xiwenAndlejian <94333236@qq.com>
2018-11-02 08:54:47 +0800

    配置文件修改：base_api => /api

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"https://www.dekuofa.com/spring"',
+  BASE_API: '"/api"',
 })
